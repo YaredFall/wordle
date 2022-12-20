57cc228484615cb027e1be8fc0a5db46d7b31618
YaredFall <yaredfall@gmail.com>
2022-12-21 04:10:28 +0700

    ~small layout and color adjustments

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -24,13 +24,16 @@ module.exports = {
 
         'text-primary-light': colors.zinc["900"],
 
-        'bg-primary-light': colors.zinc["100"],
+        'bg-primary-light': colors.zinc["200"],
         'bg-secondary-light': colors.red["300"],
         'bg-tertiary-light': colors.white,
 
         'btn-primary-light': colors.zinc["200"],
         'btn-hovered-light': colors.zinc["300"]
       },
+      screens: {
+        'tall': { 'raw': '(min-height: 800px)' }
+      }
     },
   },
   plugins: [],
