6af8d768498ab61ce63634ebf28bebcfc2df6fe6
YaredFall <yaredfall@gmail.com>
2022-12-22 21:45:38 +0700

    ~scaling adjustment

--- src/components/WordleGame/Cell.tsx
+++ src/components/WordleGame/Cell.tsx
@@ -12,7 +12,7 @@ const Cell: FC<CellProps> = ({letter, clue}) => {
     return (
         <div className={"[--accent-color:theme(colors.yellow.400)]" +
             " aspect-square border-[var(--accent-color)] border-2 flex uppercase rounded-lg" +
-            " items-center justify-center w-[clamp(3.25rem,1.5rem+5vh+5vw,2.5rem+2vh)]" +
+            " items-center justify-center w-[calc(2rem+3vmax)] sm:w-[3.25rem]" +
             ' data-[clue]:text-stone-900 data-[clue]:bg-[var(--accent-color)]' +
             ' data-[clue="0"]:[--accent-color:theme(colors.gray.600)] data-[clue="0"]:text-text-primary' +
             ' data-[clue="1"]:[--accent-color:theme(colors.gray.200)]' +
