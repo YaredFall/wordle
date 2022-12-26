0b254e4bcdac8bb843c757570506bf8c9cc00c1a
YaredFall <yaredfall@gmail.com>
2022-12-26 13:49:58 +0700

    ~cell size adjustment, hint size and pos fix and adjustment

--- src/components/WordleGame/Cell.tsx
+++ src/components/WordleGame/Cell.tsx
@@ -27,7 +27,7 @@ const Cell: FC<CellProps> = ({ letter, clue, animationDelay = 0 , hintDirection}
             >
                 <div ref={cellRef} className={"[--accent-color:theme(colors.accent-primary)] transition-colors duration-[250ms] select-none" +
                     " aspect-square border-[var(--accent-color)] border-2 flex uppercase rounded-lg hover:z-50" +
-                    " items-center justify-center w-[calc(2rem+3vmax)] sm:w-[3.25rem]" +
+                    " items-center justify-center w-[calc(2rem+3vmax)] sm:w-[3.5rem]" +
                     (clue !== undefined ? ' text-stone-900 bg-[var(--accent-color)] cursor-help' : '') +
                     (clue === 0 ? ' [--accent-color:theme(colors.accent-tertiary)!important;] text-text-primary' :
                         clue === 1 ? ' [--accent-color:theme(colors.accent-secondary)!important]' : '') +
--- src/components/WordleGame/CellHint.tsx
+++ src/components/WordleGame/CellHint.tsx
@@ -10,9 +10,9 @@ type CellHintProps = {
 const CellHint: FC<CellHintProps> = ({ clue, letter, hintDirection = "down" }) => {
 
     return (
-        <div className={"hint absolute text-xl sm:text-lg font-medium max-w-[23ch] rounded-lg lg:py-3 px-5 opacity-0 z-[60] h-[calc(2*(2rem+3vmax)-2rem)] sm:h-[5rem] transition-opacity duration-500" +
-            (hintDirection === "down" ? " top-[calc(3.75rem+3vmax)] sm:top-[calc(1.5*3.25rem-0.25rem)] lg:top-0" :
-                " top-[calc(-3.75rem-6vmax)] sm:top-[calc(-6.5rem)] lg:bottom-0 lg:top-auto") +
+        <div className={"hint absolute text-xl sm:text-lg font-medium max-w-[calc(23ch+0.75rem)] rounded-lg lg:py-3 px-6 opacity-0 z-[60] h-[calc(2*(2rem+3vmax)-2rem)] transition-opacity duration-500" +
+            (hintDirection === "down" ? " top-[calc(3.75rem+3vmax)] sm:h-[6rem] sm:top-[4.75rem] lg:top-0" :
+                " top-[calc(-3.75rem-6vmax)] sm:top-[calc(-7rem)] lg:bottom-0 lg:top-auto") +
             " lg:left-[-2rem] lg:translate-x-[-100%] lg:h-auto pointer-events-none flex items-center" +
             (clue !== undefined ? ' bg-[var(--accent-color)]' : '') +
             (clue === 0 ? ' [--accent-color:theme(colors.accent-tertiary)]' :
