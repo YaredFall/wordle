60c1aa038db01dd3682d7b4eab2973fe769ed518
YaredFall <yaredfall@gmail.com>
2023-01-10 21:56:40 +0700

    ~padding fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,13 +9,13 @@ const Navbar: FC<NavbarProps> = ({ }) => {
 
     return (
         <header className={"text-2xl sm:text-xl w-full flex flex-row items-center bg-[var(--navbar-color)] border-b border-[#00000031] transition-colors overflow-hidden"}>
-            <a className={"decoration-0 select-none cursor-pointer text-[var(--text-color)] px-3 py-5 sm:py-3 hover:text-[var(--text-color)]"}
+            <a className={"decoration-0 select-none cursor-pointer text-[var(--text-color)] p-5 sm:p-3 hover:text-[var(--text-color)]"}
                 href={"/"}
                 children={"Radish Wordle"} />
             <nav>
             
             </nav>
-            <ThemeToggle className={"group ml-auto text-[1.2em] px-3 py-5 sm:py-3 !bg-transparent border-none align-middle"} />
+            <ThemeToggle className={"group ml-auto text-[1.2em] p-5 sm:p-3 !bg-transparent border-none align-middle"} />
         </header>
     );
 };
