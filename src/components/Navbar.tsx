35446617de981af157279349851fce12f844f4ad
YaredFall <yaredfall@gmail.com>
2023-01-11 03:51:50 +0700

    ~fixed navbar link color bug; ~changed modal animation timing function

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar: FC<NavbarProps> = ({ }) => {
 
     return (
         <header className={"text-2xl sm:text-xl w-full flex flex-row items-center bg-[var(--navbar-color)] border-b border-[#00000031] transition-colors overflow-hidden"}>
-            <a className={"decoration-0 select-none cursor-pointer text-[var(--text-color)] p-5 sm:p-3 hover:text-[var(--text-color)]"}
+            <a className={"decoration-0 select-none cursor-pointer !text-[var(--text-color)] p-5 sm:p-3"}
                 href={"/"}
                 children={"Radish Wordle"} />
             <nav>
