504e8ced38d8796c52570c1970e447ebce9ee873
YaredFall <yaredfall@gmail.com>
2023-01-11 04:41:56 +0700

    testing

--- src/components/ModalMenu.tsx
+++ src/components/ModalMenu.tsx
@@ -21,8 +21,8 @@ const ModalMenu: FC<ModalMenuProps> = ({ isOpen, setIsOpen, onOpen, onClose, chi
             onEnter={onOpen}
             onExited={onClose}
         >
-            <div ref={nodeRef} className={`absolute z-[9999] w-[var(--width)] aspect-square duration-1000 transition-[clip-path] [transition-timing-function:ease] ` +
-                `[--width:250vmax] m-[calc(50%+var(--width)/-2)] inset-0 rounded-[500vmax] [clip-path:circle(0%)] ${className}`} {...otherProps}>
+            <div ref={nodeRef} className={`absolute z-[9999] w-[var(--width)] aspect-square duration-1000 transition-[clip-path] ` +
+                `[--width:400vmax] m-[calc(50%+var(--width)/-2)] inset-0 rounded-[500vmax] [clip-path:circle(0%)] ${className}`} {...otherProps}>
                 <button className={"animate-fade-in-delayed [animation-delay:250ms] opacity-0 text-2xl" +
                     " fixed top-8 right-8 p-4 aspect-square !bg-transparent hover:text-bg-secondary-light dark:hover:text-bg-secondary"}
                     onClick={() => { setIsOpen(false) }}>
